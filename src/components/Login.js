3ca0fbb93d252f51ac54fce724f4d22cbaef362f
Arup <09arup06@gmail.com>
2023-03-05 12:39:35 +0530

    UI fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,7 +30,19 @@ const Login = (props) => {
         setCredentials({...credentials,[e.target.name]: e.target.value })
     }
     return (
-        <div><form className='container my-1'>
+        
+        <div className="container my-5">
+      <div className="container h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-lg-12 col-xl-11">
+            <div className="card text-black" style={{borderRadius: "25px"}}>
+              <div className="card-body p-md-5">
+                <div className="row justify-content-center">
+                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Log In</p>
+            
+            <form className='container mx-1 mx-md-4 my-1'>
             <div className="form-group my-2">
                 <label htmlFor="email">Email</label>
                 <input type="text" className="form-control" onChange={onChange} id="email" name="email" value={credentials.email} placeholder="email" />
@@ -41,6 +53,23 @@ const Login = (props) => {
             </div>
             <button type="submit" className="btn btn-primary" onClick={handleClick}>Login</button>
         </form></div>
+        </div>
+                <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+  
+                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                    className="img-fluid" alt=""/>
+                    
+  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+ 
+  
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,30 +11,33 @@ const Navbar = (props) => {
     ]
   return (
     <div>
-       <nav className={`navbar navbar-expand-lg navbar-dark bg-dark`}>
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">Cloud NoteBook</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className={`nav-link ${location.pathname==='/'?'active':''}`} aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
+    
+<nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Cloud NoteBook</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+        <Link className={`nav-link ${location.pathname==='/'?'active':''}`} aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
               <Link className={`nav-link ${location.pathname==='/about'?'active':''}`} to="/about">About</Link>
             </li>       
-          </ul>
-          {!localStorage.getItem('logintoken')?<div>
+       
+      </ul>
+      <form className="d-flex" role="search">
+      {!localStorage.getItem('logintoken')?<div>
                   <Link className="btn btn-primary mx-2" to="/login" role="button">Login</Link>
                   <Link className="btn btn-primary mx-2" to="/register" role="button">Register</Link>
           </div>:<Link className="btn btn-primary mx-2" to="/login" onClick={logout} role="button">Logout</Link>}
-          
-        </div>
-      </div>
-    </nav>
+      </form>
     </div>
+  </div>
+</nav>
+</div>
   )
 }
 
